d7496b5db4dff41559b6f6123132dcff13d70c6e
Anwesh43 <anweshthecool0@gmail.com>
2020-04-11 06:17:06 +0530

    adding useAnimatedScale hook

---
+++ src/hooks.js
@@ -0,0 +1,21 @@
+export const useAnimatedScale = (scGap, dir) => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+    return {
+        scale,
+        start() {
+            if (!animated) {
+                var currScale  = scale
+                setAnimated(true)
+                setInterval(() => {
+                    currScale += scGap
+                    setScale(currScale)
+                    if (currScale > 1) {
+                        setScale(0)
+                        setAnimated(false)
+                    }
+                })
+            }
+        }
+    }
+}
